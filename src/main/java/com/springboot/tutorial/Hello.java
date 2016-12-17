0e7c40d21dc849f3cc7f1c806cd385851276607c
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-12-17 11:07:32 -0600

    Fixed errors in build, verified works

--- src/main/java/com/springboot/tutorial/Hello.java
+++ src/main/java/com/springboot/tutorial/Hello.java
@@ -3,13 +3,20 @@ package com.springboot.tutorial;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Hello {
 
-    @GetMapping(value="hello")
+    @RequestMapping(value="hello", method = RequestMethod.GET)
     public String getMessage(Model model) {
         model.addAttribute("message", "Hello world!");
         return "greeting";
+    }
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String getIndex() {
+        return "index";
     }
 }
